cfb120fa8ff28bf3d5999e7e18ba897f0bacc9e4
ishrajkumar <ishrajkumar781@gmail.com>
2026-08-23 21:17:46 +0530

    update routes

--- server.py
+++ server.py
@@ -19,6 +19,11 @@ def contact():
 def form():
     return render_template('form.html')
 
+@app.route(/application)
+def application():
+    return render_template("cation.html")
+
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)

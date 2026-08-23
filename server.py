75c57620ae13fbc4527ceef64dbaf5af77a1d9d8
ishrajkumar <ishrajkumar781@gmail.com>
2026-08-23 20:41:22 +0530

    restore all website routes

--- server.py
+++ server.py
@@ -7,6 +7,18 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
